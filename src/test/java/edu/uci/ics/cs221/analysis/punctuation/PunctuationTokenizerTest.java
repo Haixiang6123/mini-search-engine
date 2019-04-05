e4302d52d6a6cfb7ce06a1be2cc1bcf1814a153b
Haixiang <haixiang6123@gmail.com>
2019-04-05 10:03:08 -0700

    Update test case name

--- src/test/java/edu/uci/ics/cs221/analysis/punctuation/PunctuationTokenizerTest.java
+++ src/test/java/edu/uci/ics/cs221/analysis/punctuation/PunctuationTokenizerTest.java
@@ -9,14 +9,17 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class PunctuationTokenizerTest {
-
+    public PunctuationTokenizerTest() {
+        System.out.println("Describe: PunctuationTokenizer");
+    }
     @Test
-    public void test1() {
+    public void testFunctionality() {
+        System.out.println("It: can tokenize normal string with white spaces");
+
         String text = "I am Happy Today!";
         List<String> expected = Arrays.asList("happy", "today");
         PunctuationTokenizer tokenizer = new PunctuationTokenizer();
 
         assertEquals(expected, tokenizer.tokenize(text));
     }
-
 }
