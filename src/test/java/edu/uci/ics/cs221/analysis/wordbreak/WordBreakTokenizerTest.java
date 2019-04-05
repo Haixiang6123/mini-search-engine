abd71aa3af59803318fbe45055366536e54b9614
Haixiang <haixiang6123@gmail.com>
2019-04-05 13:57:07 -0700

    Add test case for not breakable tokenize

--- src/test/java/edu/uci/ics/cs221/analysis/wordbreak/WordBreakTokenizerTest.java
+++ src/test/java/edu/uci/ics/cs221/analysis/wordbreak/WordBreakTokenizerTest.java
@@ -3,6 +3,7 @@ package edu.uci.ics.cs221.analysis.wordbreak;
 import edu.uci.ics.cs221.analysis.WordBreakTokenizer;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,13 +12,20 @@ import static org.junit.Assert.assertEquals;
 public class WordBreakTokenizerTest {
 
     @Test
-    public void test1() {
+    public void testCanBreak() {
         String text = "catdog";
         List<String> expected = Arrays.asList("cat", "dog");
         WordBreakTokenizer tokenizer = new WordBreakTokenizer();
 
         assertEquals(expected, tokenizer.tokenize(text));
-
     }
 
+    @Test
+    public void testCanNotBreak() {
+        String text = "xzy";
+        List<String> expected = new ArrayList<>();
+
+        WordBreakTokenizer tokenizer = new WordBreakTokenizer();
+        assertEquals(expected, tokenizer.tokenize(text));
+    }
 }
