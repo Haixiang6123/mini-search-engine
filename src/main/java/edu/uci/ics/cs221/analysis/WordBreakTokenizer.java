f2bed12e3e1275dfcd7895c7a2bd3d25ba15c886
Haixiang <haixiang6123@gmail.com>
2019-04-09 16:06:11 -0700

    Add method to compute probabilities

--- src/main/java/edu/uci/ics/cs221/analysis/WordBreakTokenizer.java
+++ src/main/java/edu/uci/ics/cs221/analysis/WordBreakTokenizer.java
@@ -35,7 +35,7 @@ import java.util.*;
  *
  */
 public class WordBreakTokenizer implements Tokenizer {
-    private Map<String, Long> dict = null;
+    private Map<String, Double> dict = null;
     private Set<String> dictTokens = null;
 
     class Result {
@@ -66,14 +66,13 @@ public class WordBreakTokenizer implements Tokenizer {
         for (String line : lines) {
             // Init item
             String[] item = line.trim().split(" ");
-            dict.put(item[0], Double.valueOf(item[1]));
-            // Compute total
-            total += Double.valueOf(item[1]);
-            // Prevent from empty string
             if (item.length != 2) {
                 continue;
             }
-            dict.put(item[0], Long.valueOf(item[1]));
+            dict.put(item[0], Double.valueOf(item[1]));
+            // Compute total
+            total += Double.valueOf(item[1]);
+            dict.put(item[0], Double.valueOf(item[1]));
         }
         dictTokens = dict.keySet();
 
