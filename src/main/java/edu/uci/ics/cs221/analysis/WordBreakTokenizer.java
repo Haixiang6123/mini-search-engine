d9b689d6bda4c6bdeedeb1b88c642136162265a8
Haixiang <haixiang6123@gmail.com>
2019-04-10 09:47:49 -0700

    Add probability compare feature

--- src/main/java/edu/uci/ics/cs221/analysis/WordBreakTokenizer.java
+++ src/main/java/edu/uci/ics/cs221/analysis/WordBreakTokenizer.java
@@ -9,30 +9,29 @@ import java.util.*;
 
 /**
  * Project 1, task 2: Implement a Dynamic-Programming based Word-Break Tokenizer.
- *
+ * <p>
  * Word-break is a problem where given a dictionary and a string (text with all white spaces removed),
  * determine how to break the string into sequence of words.
  * For example:
  * input string "catanddog" is broken to tokens ["cat", "and", "dog"]
- *
+ * <p>
  * We provide an English dictionary corpus with frequency information in "resources/cs221_frequency_dictionary_en.txt".
  * Use frequency statistics to choose the optimal way when there are many alternatives to break a string.
  * For example,
  * input string is "ai",
  * dictionary and probability is: "a": 0.1, "i": 0.1, and "ai": "0.05".
- *
+ * <p>
  * Alternative 1: ["a", "i"], with probability p("a") * p("i") = 0.01
  * Alternative 2: ["ai"], with probability p("ai") = 0.05
  * Finally, ["ai"] is chosen as result because it has higher probability.
- *
+ * <p>
  * Requirements:
- *  - Use Dynamic Programming for efficiency purposes.
- *  - Use the the given dictionary corpus and frequency statistics to determine optimal alternative.
- *      The probability is calculated as the product of each token's probability, assuming the tokens are independent.
- *  - A match in dictionary is case insensitive. Output tokens should all be in lower case.
- *  - Stop words should be removed.
- *  - If there's no possible way to break the string, throw an exception.
- *
+ * - Use Dynamic Programming for efficiency purposes.
+ * - Use the the given dictionary corpus and frequency statistics to determine optimal alternative.
+ * The probability is calculated as the product of each token's probability, assuming the tokens are independent.
+ * - A match in dictionary is case insensitive. Output tokens should all be in lower case.
+ * - Stop words should be removed.
+ * - If there's no possible way to break the string, throw an exception.
  */
 public class WordBreakTokenizer implements Tokenizer {
     private Map<String, Double> dict = null;
@@ -41,6 +40,7 @@ public class WordBreakTokenizer implements Tokenizer {
     class Result {
         public boolean breakable = false;
         public List<String> tokens = new ArrayList<>();
+        public double probability = 0;
 
         @Override
         public String toString() {
@@ -92,42 +92,66 @@ public class WordBreakTokenizer implements Tokenizer {
         if (text == null || text.equals("")) {
             return new ArrayList<>();
         }
+        Result[][] matrix = new Result[text.length()][text.length()];
+
         // Pre-process text
         text = text.trim().toLowerCase();
+
+        // Break pre-processed text
+        breakWord(matrix, text);
+
+        // Return final result: 0 -> text length - 1
+        return matrix[0][text.length() - 1].tokens;
+    }
+
+    private void breakWord(Result[][] matrix, String text) {
         int n = text.length();
-
-        Result[][] matrix = new Result[n][n];
-
-        // n: text length
-        // Window size: 1 to n
         for (int window = 1; window <= n; window++) {
-            // Start index: 0 to n - window size
+            // Start index: 0 -> n - window size
             for (int start = 0; start <= n - window; start++) {
-                Result result = new Result();
                 int end = start + window - 1;
                 String subText = text.substring(start, end + 1);
+                Result result = new Result();
 
-                if (dictTokens.contains(subText)) {
-                    result.breakable = true;
-                    result.tokens.add(subText);
-                }
-                else {
-                    for (int middle = start; middle < end; middle++) {
-                        if (isBreakable(matrix, start, middle, end)) {
-                            Result left = matrix[start][middle];
-                            Result right = matrix[middle + 1][end];
-                            result.tokens.addAll(left.tokens);
-                            result.tokens.addAll(right.tokens);
-                            result.breakable = true;
-                            // todo: need to consider the probability
-                            break;
-                        }
-                    }
-                }
+                // Check sub text whether exist in dictionary
+                checkDict(result, subText, dict);
+
+                // Compare with previous sub text probabilities
+                comparePrevSubText(result, start, end, matrix);
+
                 matrix[start][end] = result;
             }
         }
-        return matrix[0][n - 1].tokens;
+    }
+
+    private void checkDict(Result result, String subText, Map<String, Double> dict) {
+        if (dictTokens.contains(subText)) {
+            result.breakable = true;
+            result.tokens.add(subText);
+            // Init probability for this sub text
+            result.probability = dict.get(subText);
+        }
+    }
+
+    private void comparePrevSubText(Result result, int start, int end, Result[][] matrix) {
+        for (int middle = start; middle < end; middle++) {
+            if (isBreakable(matrix, start, middle, end)) {
+                Result left = matrix[start][middle];
+                Result right = matrix[middle + 1][end];
+                double curProb = left.probability * right.probability;
+                if (curProb > result.probability) {
+                    updateResult(result, left, right, curProb);
+                }
+            }
+        }
+    }
+
+    private void updateResult(Result result, Result left, Result right, double curProb) {
+        result.probability = curProb;
+        result.tokens.clear();
+        result.tokens.addAll(left.tokens);
+        result.tokens.addAll(right.tokens);
+        result.breakable = true;
     }
 
     // Check if it can be broken it by given start, end index
