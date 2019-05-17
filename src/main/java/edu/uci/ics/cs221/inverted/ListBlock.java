213135fcc79d779cec01da5341a36b95528d2dab
Haixiang <haixiang6123@gmail.com>
2019-05-17 13:34:44 -0700

    Add ListBlock

---
+++ src/main/java/edu/uci/ics/cs221/inverted/ListBlock.java
@@ -0,0 +1,15 @@
+package edu.uci.ics.cs221.inverted;
+
+import java.util.List;
+
+public class ListBlock {
+    public byte[] encodedInvertedList = null;
+    public byte[] encodedGlobalOffsets = null;
+    public List<Integer> invertedList = null;
+    public List<Integer> globalOffsets = null;
+
+    public ListBlock(int listLength, int globalOffsetLength) {
+        this.encodedInvertedList = new byte[listLength];
+        this.encodedGlobalOffsets = new byte[globalOffsetLength];
+    }
+}
