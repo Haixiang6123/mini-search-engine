87186ebe4b8a928e7e38e0bfee7121dc5c00ad99
hanluc <hanluc@uci.edu>
2019-05-17 10:49:50 -0700

    Reorganize comments & delete print out.

--- src/main/java/edu/uci/ics/cs221/positional/DeltaVarLenCompressor.java
+++ src/main/java/edu/uci/ics/cs221/positional/DeltaVarLenCompressor.java
@@ -21,9 +21,7 @@ public class DeltaVarLenCompressor implements Compressor {
         gaps[0] = integers.get(0);
         for(int i = integers.size() - 1; i >= 1; i-- ) {
             gaps[i] = integers.get(i) - integers.get(i-1);
-//            System.out.println(gaps[i] + " = "+ integers.get(i) + " - " + integers.get(i-1));
         }
-        System.out.println("Gaps:" + Arrays.toString(gaps));
 
         //Process bytes:
         List<Byte> codes = new ArrayList<>();
@@ -32,27 +30,23 @@ public class DeltaVarLenCompressor implements Compressor {
             int num = i;
             int slice = 0;
             while(num > 0){
-                // Get lowest 7 bit & shift num by 7 bit
+                // Get lowest 7 bit & shift num
                 slice = num & 0x7F;
                 num >>= 7;
 
-                System.out.println(slice);
-
-                // Put 7 bit slice in encode array
-                if (encodeParts.size() > 0)     // Put lowest bit in the byte.
+                // Put slice(7 bit) in encode array
+                if (encodeParts.size() > 0)
                     slice += (1 << 7);
-                encodeParts.add((byte)slice);     //add(0, x) does not work?
+                encodeParts.add((byte)slice);
                 slice = 0;
             }
 
-            System.out.println(slice);
-            if (encodeParts.size() == 0)   // non added : num was 0
+            // Add Num 0: haven't been processed
+            if (encodeParts.size() == 0)
                 encodeParts.add((byte)slice);
 
-//            System.out.println(encodeParts);  // before reverse
-
+            // Reverse list and add this number's code to bytes.
             Collections.reverse(encodeParts);
-            System.out.println("Encoded bytes:" + encodeParts);
             codes.addAll(encodeParts);
         }
 
@@ -73,18 +67,22 @@ public class DeltaVarLenCompressor implements Compressor {
         List<Integer> result = new ArrayList<>();
         int num = 0;
         for(int i = start; i < start + length; i++){
-            int newInt = bytes[i] & 0xFF;  // Get unsigned number
-            if(newInt > 127)  // 1 on higest bit todo get bit from byte directly
-                num = (num << 7) + (newInt & 0x7F); // delete 1 & concatenate last 7 bit
-            else {           // 0 on highest bit
-                num = (num << 7) + newInt;     // num[] + newInt[1:]
-                System.out.println("decoded num: " + num);
+            // Get unsigned number
+            int newInt = bytes[i] & 0xFF;
+            // 1 on higest bit : still need to go
+            if(newInt > 127) {
+                // Delete 1 & concatenate last 7 bit
+                num = (num << 7) + (newInt & 0x7F);
+            }
+            // 0 on highest bit : a number ends
+            else {
+                // Directly concatenate last 7 bit & push back decoded number & reset
+                num = (num << 7) + newInt;
                 result.add(num);
                 num = 0;
             }
         }
 
-        System.out.println(" Decodes gaps: " + result);
         // Fill the gap
         for(int i = 1 ; i < result.size(); i++ ){
             result.set(i, result.get(i) + result.get(i -1));
