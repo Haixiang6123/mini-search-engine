73b610b766ae342b33a6297e2d20a344fd911655
Haixiang <haixiang6123@gmail.com>
2019-06-04 10:28:15 -0700

    Add File Utils

---
+++ src/main/java/utils/FileUtils.java
@@ -0,0 +1,28 @@
+package utils;
+
+import edu.uci.ics.cs221.storage.Document;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class FileUtils {
+    public static String readFileAsString(File file, ReadFileCallback callback) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Build a string
+                stringBuilder.append(line);
+                // Read file callback
+                callback.callback(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return stringBuilder.toString();
+    }
+}
